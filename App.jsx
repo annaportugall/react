fc47e0018a36baf1ce78944d035ef7fb6484cfb7
annaportugall <annaclaraazeredo@gmail.com>
2025-11-18 09:49:59 -0300

    e

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function BemVindo(){
+  return(
+  <h1>Bem BemVindo ao React!</h1>
+  )
+}
+
+export default BemVindo;  
