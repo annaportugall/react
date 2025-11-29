a5f359e2fd708ab89e2604061207073f8d97705e
annaportugall <annaclaraazeredo@gmail.com>
2025-11-29 13:43:10 -0300

    IGYEIH

--- annao-react/src/App.jsx
+++ annao-react/src/App.jsx
@@ -1,62 +1,23 @@
-import { useState } from "react";
+import { useState } from 'react';
 import './App.css'
 
-
 function Calculadora() {
-  const [n1, setN1] = useState("");
-  const [n2, setN2] = useState("");
-  const [operacao, setOperacao] = useState("adicao");
-  const [resultado, setResultado] = useState(null);
+  const [n1, setN1] = useState('');
+  const [n2, setN2] = useState('');
 
-  const calcular = () => {
-    const num1 = parseFloat(n1);
-    const num2 = parseFloat(n2);
-    let res;
-
-    if (operacao === "adicao") {
-      res = num1 + num2;
-    } else if (operacao === "subtracao") {
-      res = num1 - num2;
-    } else if (operacao === "multiplicacao") {
-      res = num1 * num2;
-    } else if (operacao === "divisao") {
-      res = num1 / num2;
-    }
-
-    setResultado(res);
+  const calcular = (operacao) => {
+    let res = Number(n1) * Number(n2);
+ 
+    document.getElementById("resul").innerText = `Resultado: ${res}`;
   };
 
   return (
     <div>
-      <h1>Calculadora</h1>
-
-      <select value={operacao} onChange={(e) => setOperacao(e.target.value)}>
-        <option value="adicao">+</option>
-        <option value="subtracao">-</option>
-        <option value="multiplicacao">*</option>
-        <option value="divisao">/</option>
-      </select>
-      <br />
-
-      Número 1:{" "}
-      <input
-        type="number"
-        value={n1}
-        onChange={(e) => setN1(e.target.value)}
-      />
-      <br />
-
-      Número 2:{" "}
-      <input
-        type="number"
-        value={n2}
-        onChange={(e) => setN2(e.target.value)}
-      />
-      <br />
-
-      <button onClick={calcular}>Calcular</button>
-
-      {resultado !== null && <h2>Resultado: {resultado}</h2>}
+      <h2>Calcular a Área</h2>
+      <input type="number" value={n1} onChange={(e) => setN1(e.target.value)} placeholder="Altura" />
+      <input type="number" value={n2} onChange={(e) => setN2(e.target.value)} placeholder="Comprimento" />
+      <button onClick={() => calcular()}>Calcular</button>
+      <h2 id="resul">Resultado: </h2>
     </div>
   );
 }
